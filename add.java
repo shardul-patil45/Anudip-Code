ec908b215fb23b352d78c7a6751000e610947dd6
shardul-patil45 <patilshardul40@gmail.com>
2025-06-25 13:35:18 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class add{
+    int add(){
+    int a = 10;
+    int b = 20;
+    int sum = a+b;
+    return sum;
+    }
+
+    public static void main(String[] args) {
+        
+        add h1 = new add();
+        int sum = h1.add();
+        System.out.println(sum);
+}
+}
---
+++ product.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class prod{
+    int ok(){
+    int a = 10;
+    int b = 20;
+    int c = a*b;
+    return c;
+    }
+
+    public static void main(String[] args) {
+        
+        prod h1 = new prod();
+        int abc = h1.ok();
+        System.out.println(abc);
+}
+}
